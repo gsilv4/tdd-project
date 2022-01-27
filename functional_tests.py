2640d04b6871ed32a2718b8d636bac5ef5719668
Gabriel Silva <gabriel.silva@dcc.ufmg.br>
2022-01-26 21:31:54 -0300

    Ciclo: Teste de Unidade/Desenvolvimento

--- functional_tests.py
+++ functional_tests.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from selenium import webdriver
 import unittest
 
@@ -9,7 +10,7 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):  
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):  
+    def test_can_start_a_list_and_retrieve_it_later(self):
         # Maria decidiu utilizar o novo app TODO. Ela entra em sua página principal:
         self.browser.get('http://localhost:8000')
 
